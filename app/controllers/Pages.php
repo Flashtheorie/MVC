2514878ad2116598d9b6a55b4892ec781ba5ce07
Antoine <antoinepascualmail@gmail.com>
2021-02-06 19:28:41 +0100

    Template final

---
+++ app/controllers/Pages.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Every page loads from view folder
+ * in order to load a view inside a folder of the view folder
+ * the folder/filename must be parsed
+ */
+class Pages extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+    public function about($id)
+    {
+        echo 'hello' . $id;
+        # code...
+    }
+    public function Index(Type $var = null)
+    {
+        
+        $data = [
+            'title' => "My MVC Framework",
+        ];
+        $this->view('pages/index', $data);
+    }
+}
